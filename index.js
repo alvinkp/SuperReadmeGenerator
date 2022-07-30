d7d959627f5a415e71e770038ae63afdee5df133
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-07-30 19:07:49 -0400

    Updated Javascript
    *JS*
    
    - Added more questions
    - Updated writeToFile()
    - Updated tableOfContents()
    - Updated projectDesc()
    - Updated projectTitle()
    - Updated init()

--- index.js
+++ index.js
@@ -5,40 +5,73 @@ const generateMD = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    'What is the name of your project?',
-    'What license would you like to use?',
-    'What is your project about?',
+    {type:'input',message:'What is the name of your project?',name:'pTitle'},
+    {type:'input',message:'Please enter a short description of your project:',name:'pDesc'},
+    {type:'input',message:'What was your motivation for creating this project?',name:'pMotivation'},
+    {type:'input',message:'What problem does your project solve?',name:'pProblem'},
+    {type:'input',message:'What have you learned from working on this project?',name:'pLearnings'},
+    {type:'confirm',message:'Do you want to include Installation Instructions?',name:'pInstructions'},
+    {type:'confirm',message:'Do you want to include usage information?',name:'pUsage'},
+    {type:'confirm',message:'Do you want to include Credit(s)?',name:'pCredits'},
+    {type:'confirm',message:'Do you want to include License(s)?',name:'pLicense'},
 ];
 
+
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data)
+}
 
 
 // Create Table of Contents
 let tableOfContents = (installation, usage, credits, license) => {
-    return 
+    return `## <span style="color:#FFD23F">Table of Contents</span>\n\r
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [Credits](#credits)
+        - [License](#license)\r
+        ## <span style="color:#FFD23F">Installation</span>\n\r
+        ${installation}\r
+        ## <span style="color:#FFD23F">Usage</span>\n\r
+        ${usage}\r
+        ## <span style="color:#FFD23F">Credits</span>\n\r
+        ## SCREENSHOTS GO HERE\r
+        ${credits}\r
+        ## <span style="color:#FFD23F">License</span>\n\r
+        ${license}\r`
 }
 
 
 // Create Description
-let projectDesc = (title, desc, motivation, intention, problem, learnings) => {
-    return [
-        `## <span style="color:#FFD23F">${title}</span>\n`,
-        `${desc}\n`,
-        `- ${motivation}`,
-        `- ${intention}`,
-        `- ${problem}`,
-        `- ${learnings}\n`
-    ]
+let projectDesc = (desc, motivation, problem, learnings) => {
+    return `## <span style="color:#FFD23F">Description</span>\n
+        ${desc}\r\n
+        - ${motivation}
+        - ${problem}
+        - ${learnings}\r`
 }
 
 // Create Project Title
-let projectTitle = (title, desc) => {
-    return [`# <span style="color:#FFD23F">${title}</span>\n`, desc]
+let projectTitle = (title) => {
+    return `# <span style="color:#FFD23F">${title}</span>\n\r`
 }
 
+
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    // Ask User for input using inquirer
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+            let myReadmeElements = [];
+            myReadmeElements.push(projectTitle(answers.pTitle));
+            myReadmeElements.push(projectDesc(answers.pDesc, answers.pMotivation, answers.pProblem, answers.pLearnings));
+            writeToFile("myReadme.md", myReadmeElements);
+        })
+}
 
 // Function call to initialize app
 init();
