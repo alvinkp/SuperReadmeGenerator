97e1d9a533770e0f93428f9d6e19cfda52e6a1fd
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-08-03 09:15:53 -0400

    Update Index.js & generateMarkdown.js
    *JS*
    
    -- INDEX.JS --
    - Updated questions array
    - removed vestigial code
    
    -- GENERATEMARKDOWN.JS --
    - Updated renderLicenseSection()
    - Updated Markdown in generateMarkdown()

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
 const fs = require('fs/promises');
 const inquirer = require('inquirer');
-const generateMD = require('./utils/generateMarkdown.js');
+const generateMD = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -10,9 +11,13 @@ const questions = [
     { type: 'input', message: 'What was your motivation for creating this project?', name: 'pMotivation' },
     { type: 'input', message: 'What problem does your project solve?', name: 'pProblem' },
     { type: 'input', message: 'What have you learned from working on this project?', name: 'pLearnings' },
+    { type: 'input', message: 'What are your Installation Instructions?', name: 'pInstructions' },
+    { type: 'input', message: 'What is the Usage Information you would like to include?', name: 'pUsage' },
+    { type: 'input', message: 'How can someone contribute to your project?', name: 'pContributing' },
+    { type: 'input', message: 'What are your test instructions?', name: 'pTests' },
+    { type: 'input', message: 'How can someone reach you if they have questions? Please enter your information like this without parentheses (Github Username, Email Address)', name: 'pQuestions' },
     { type: 'list', message: 'Which License would you like to include?', name: 'pLicense', choices: ["Apache 2.0 License", "Boost", "BSD 3-Clause License", "BSD 2-Clause License", "CC0", "Attribution 4.0 International", "Attribution-ShareAlike 4.0 International", "Attribution-NonCommercial 4.0 International", "Attribution-NoDerivates 4.0 International", "Attribution-NonCommmercial-ShareAlike 4.0 International", "Attribution-NonCommercial-NoDerivatives 4.0 International", "Eclipse Public License 1.0", "GNU GPL v3", "GNU GPL v2", "GNU AGPL v3", "GNU LGPL v3", "GNU FDL v1.3", "The Hippocratic License 2.1", "The Hippocratic License 3.0", "IBM Public License Version 1.0", "ISC License (ISC)", "The MIT License", "Mozilla Public License 2.0", "Attribution License (BY)", "Open Database License (ODbL)", "Public Domain Dedication and License (PDDL)", "The Perl License", "The Artistic License 2.0", "SIL Open Font License 1.1", "The Unlicense", "The Do What the Fuck You Want to Public License", "The zlib/libpng License"]}
 ];
-
 
 
 // TODO: Create a function to write README file
@@ -21,30 +26,13 @@ function writeToFile(fileName, data) {
 }
 
 
-// Create Description
-let projectDesc = (desc, motivation, problem, learnings) => {
-    return `## <span style="color:#FFD23F">Description</span>\n
-        ${desc}\r\n
-        - ${motivation}
-        - ${problem}
-        - ${learnings}\r`
-}
-
-// Create Project Title
-let projectTitle = (title) => {
-    return `# <span style="color:#FFD23F">${title}</span>\n\r`
-}
-
-
 // TODO: Create a function to initialize app
 function init() {
-
     // Ask User for input using inquirer
     inquirer
         .prompt(questions)
         .then((answers) => {
-            console.log(answers);
-            writeToFile("myReadme.md", generateMD.generateMarkdown(answers));
+            writeToFile("myReadmeExample.md", generateMD.generateMarkdown(answers));
         })
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -278,19 +278,51 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `This project uses the ${renderLicenseBadge(license)} license, you can find more information here: ${renderLicenseLink(license)}.`
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+
+  let split = data.pQuestions.split(",");
   return `
-  # <span style="color:#FFD23F">${data.pTitle} </span> 
+  # <span style="color:#FFD23F">${data.pTitle} </span> ${renderLicenseBadge(data.pLicense)}\n\r 
   ## <span style="color:#FFD23F">Description</span>\n
         ${data.pDesc}\r\n
         - Motivation: ${data.pMotivation}
         - Solves: ${data.pProblem}
         - Lessons: ${data.pLearnings}\r
+  ---
+  ## <span style="color:#FFD23F">Table of Contents</span>\n
+  - [Installation](#installation-instructions)
+  - [Usage](#usage)
+  - [Contributing](#how-to-contribute)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [License](#license)\r
+  ---
+  ## <span style="color:#FFD23F">Installation Instructions</span>\n
+  ${data.pInstructions}\n
+  ---
+  ## <span style="color:#FFD23F">Usage</span>\n
+  ${data.pUsage}\n
+  ---
+  ## <span style="color:#FFD23F">How to Contribute</span>\n
+  ${data.pContributing}\n
+  ---
+  ## <span style="color:#FFD23F">Tests</span>\n
+  ${data.pTests}\n
+  ---
+  ## <span style="color:#FFD23F">Questions</span>\n
+  Github: [@${split[0]}](https://www.github.com/${split[0]})\n
+  Email: ${split[1]}\n
+  ---
+  ## <span style="color:#FFD23F">License</span>\n
+  ${renderLicenseSection(data.pLicense)}
 `;
 }
+
 
 // module.exports = generateMarkdown;
 exports.generateMarkdown = generateMarkdown;
