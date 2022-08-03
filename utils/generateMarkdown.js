1762149514feae197de33daf8029da0c19a8dd78
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-08-03 14:09:20 -0400

    Update JS
    Added No license as an option

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,6 +3,10 @@
 function renderLicenseBadge(license) {
   let badgeImageUrl;
   switch (license) {
+    case "No License":
+      badgeImageUrl = ""
+      break;
+
     case "Apache 2.0 License":
       badgeImageUrl = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
       break;
@@ -142,6 +146,11 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   let licenseLink;
   switch (license) {
+    
+    case "No License":
+      badgeImageUrl = ""
+      break;
+
     case "Apache 2.0 License":
       licenseLink = "https://opensource.org/licenses/Apache-2.0";
       break;
@@ -279,7 +288,10 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return `This project uses the ${renderLicenseBadge(license)} license, you can find more information here: ${renderLicenseLink(license)}.`
+  if(license){
+    return `This project uses the ${renderLicenseBadge(license)} license, you can find more information here: ${renderLicenseLink(license)}.`
+  }
+  return "No License has been assigned to this project.";
  }
 
 // TODO: Create a function to generate markdown for README
